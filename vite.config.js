532616df3dc9ffb0facd60479cfbfebf638a4267
shivamgulati <shivam.gulati@thoughtworks.com>
2023-05-13 01:08:08 +0530

    000: Shivam [Configure vitest for unit test and coverage]

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      statements: 90,
+      include: ['src/**/*'],
+      all: true
+    }
+  },
 })
