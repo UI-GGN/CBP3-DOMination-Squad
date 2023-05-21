3eb85462590430fe44105c51c0fd0621cc9c0cb8
Shivansh Garg <shivanshgarg@Shivanshs-MacBook-Pro-2.local>
2023-05-21 17:51:28 +0530

    [Shivansh] Add test stage in config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +12,7 @@ export default defineConfig({
       reporter: ['text', 'json', 'html'],
       statements: 90,
       include: ['src/**/*'],
-      all: true
-    }
+      all: true,
+    },
   },
-})
+});
