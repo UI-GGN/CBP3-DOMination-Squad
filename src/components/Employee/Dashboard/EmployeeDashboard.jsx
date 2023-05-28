da4a31c9f11ed996a9068644e5d5d49b58cfc064
MalanB <malan.kmu@gmail.com>
2023-05-29 01:06:14 +0530

    Remove unwanted comments
    Co-authored-by: sonali-8987 <sonalikumari141999@gmail.com

--- src/components/Employee/Dashboard/EmployeeDashboard.jsx
+++ src/components/Employee/Dashboard/EmployeeDashboard.jsx
@@ -13,23 +13,13 @@ const data = [
   {
     id: 2,
     tabValue: '2',
-    tabTitle: 'Cab Requests by you',
+    tabTitle: 'Requested Cabs',
     tabContent: <Requests type="employee"/>,
   },
 ];
 
 const EmployeeDashboard = () => {
   return (
-    // <Container>
-    //   <div className="flex flex-row h-10 ml-3 w-center justify-start">
-    //     <div className="text-base mt-2 mr-2">onTrack</div>
-    //     <img className="h-20 w-20 mr-2" src={Logo} alt="logo" />
-    //   </div>
-    //
-    //   <div className="flex flex-col justify-between items-center">
-    //     <ScheduleCabDialogBox />
-    //   </div>
-    // </Container>
     <div className="flex flex-col h-screen">
       <Header />
       <CustomTabs data={data} />
