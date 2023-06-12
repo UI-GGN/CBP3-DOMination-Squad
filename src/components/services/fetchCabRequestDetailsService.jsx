dd32954207b61abb026e2e54571f8238db0183e4
MalanB <malan.kmu@gmail.com>
2023-06-12 10:49:02 +0530

    Modify fetchCabRequestDetails URL to reflect correct endpoint

--- src/components/services/fetchCabRequestDetailsService.jsx
+++ src/components/services/fetchCabRequestDetailsService.jsx
@@ -5,6 +5,6 @@ export default {
   fetchCabRequestDetails: (payload) => {
     axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
     axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-    return axios.get(`${API_URL.cabRequest}`, payload);
+    return axios.get(`${API_URL.cabRequest}/employee/12345`, payload);
   },
 };
