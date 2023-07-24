6e02e24d548b3dd92bfff10640decf3dace87bb6
Shivansh Garg <shivanshgarg@Shivanshs-MacBook-Pro-2.local>
2023-07-24 11:14:07 +0530

    [Shivansh] Fix loader alignment

--- src/components/Admin/Requests/Request.style.js
+++ src/components/Admin/Requests/Request.style.js
@@ -16,7 +16,7 @@ export const Loader = styled.div`
   align-items: center;
   position: absolute;
   left: 50%;
-  top: 50%;
+  top: 60%;
   transform: translate(-50%, -50%);
 
   @media (max-width: 800px) {
